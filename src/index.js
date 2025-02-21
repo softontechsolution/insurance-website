f99f0cea039e76b0e29eac4bbab93ebcb16aebb7
softontechsolution <softontechsolution@gmail.com>
2025-02-21 11:18:05 +0100

    dependency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+// Import Swiper styles
+import 'swiper/css';
+import 'aos';
+import 'aos/dist/aos.css';
 import reportWebVitals from './reportWebVitals';
+import 'react-tooltip/dist/react-tooltip.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-modern-drawer/dist/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
