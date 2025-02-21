9c76b0c5f2927d1897a2ab1285e63d9b6c89429a
softontechsolution <softontechsolution@gmail.com>
2025-02-21 12:07:11 +0100

    home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./screens/Login";
+import Home from './screens/Home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
