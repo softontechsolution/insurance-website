36bd1023ba492497ac70785b49a5888a1356da06
softontechsolution <softontechsolution@gmail.com>
2025-02-21 12:29:48 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+      <h1 className='w-full text-3xl font-bold'>QUALITY INSURANCE</h1>
+      <ul className='flex'>
+        <li className='p-4'>Buy a Policy</li>
+        <li className='p-4'>Buy on Whatsapp</li>
+        <li className='p-4'>Investor Relations</li>
+        <li className='p-4'>Benefit Calculator</li>
+        <li className='p-4'>About Us</li>
+        <li className='p-4'>Privacy Policy</li>
+      </ul>
+    </div>
   )
 }
 
